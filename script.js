bb2070d6fc640119bebbbc78637108294458748d
Shruthi-10 <150574359+Shruthi-10@users.noreply.github.com>
2024-06-17 16:30:26 -0700

    Third commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const createPet = function (name, species) {
             this.isTired = 1;
         },
 
-        play: function() {
+        play: function () {
             if (this.isTired === 10) {
                 console.log(`Too tired to play.`);
                 this.sleep();
@@ -26,17 +26,48 @@ const createPet = function (name, species) {
     return pet;
 };
 
-const sora = createPet ("Sora","ferret");
-const clover = createPet ("Clover", "rabbit");
-const baxter = createPet ("Baxter", "hamster");
-const cleo = createPet ("Cleo", "rat");
-const francine = createPet ("Francine", "turtle");
+const sora = createPet("Sora", "ferret");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter", "hamster");
+const cleo = createPet("Cleo", "rat");
+const francine = createPet("Francine", "turtle");
 
-/* console.log(sora, clover, baxter, cleo, francine); */
-clover.sleep();
+/* console.log(sora, clover, baxter, cleo, francine); 
 baxter.play();
 
-console.log(clover, baxter);
+console.log(clover, baxter); 
+clover.sleep(); */
 
+/* Update Properties & Create an Array of Objects */
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+console.log(allPets);
+
+// Display Pets in the Browser
+const showPets = function (petArray) {
+    pets.innerHTML = "";
+
+    for (let pet of petArray) {
+        let status = "ready to play!";
+
+        if (pet.isTired >= 7) {
+            status = "sleeping.";
+        }
+
+        const li = document.createElement("li");
+        li.innerHTML = `<span class ="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+        pets.append(li);
+    }
+};
+
+//Add Click Event
+
+statusButton.addEventListener(
+    "click",
+    function () {
+        showPets(allPets);
+    });
 
 
